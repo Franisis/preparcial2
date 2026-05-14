886f7d1ff0e9b0b2cd3edddc8c90010e8c6531b5
Franisis <gusmanfrancisco199@gmail.com>
2026-05-14 11:51:18 -0500

    db conn + countries

---
+++ src/countries/countries.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+
+import { Country } from './entities/country.entity';
+
+@Injectable()
+export class CountriesService {
+
+  constructor(
+    @InjectRepository(Country)
+    private countryRepository: Repository<Country>,
+  ) {}
+
+  async findAll() {
+    return this.countryRepository.find();
+  }
+}
